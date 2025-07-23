e66436209766034dbcf85e327935bacd4d4fc135
Hamza-ye <7amza.it@gmail.com>
2025-07-23 21:09:13 +0300

    remove unused imports

--- lib/commons/custom_widgets/async_value.widget.dart
+++ lib/commons/custom_widgets/async_value.widget.dart
@@ -1,6 +1,3 @@
-import 'package:datarunmobile/features/form_submission/application/element/form_element.dart';
-import 'package:datarunmobile/features/form_submission/application/element/form_element_state.dart';
-import 'package:datarunmobile/features/form_submission/presentation/field/field.widget.dart';
 import 'package:datarunmobile/features/form_ui_elements/presentation/get_error_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
--- lib/features/form_submission/presentation/field/field.widget.dart
+++ lib/features/form_submission/presentation/field/field.widget.dart
@@ -1,8 +1,6 @@
 import 'package:d_sdk/core/logging/new_app_logging.dart';
 import 'package:datarunmobile/features/form_submission/application/element/form_element.dart';
-import 'package:datarunmobile/features/form_submission/application/form_instance.provider.dart';
 import 'package:datarunmobile/features/form_submission/application/form_widget_factory.dart';
-import 'package:datarunmobile/features/form_submission/presentation/widgets/form_metadata_inherit_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';

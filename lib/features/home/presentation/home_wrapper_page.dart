a9073cff75adbec6a30cbe3b39a739f9b4375438
Hamza-ye <7amza.it@gmail.com>
2026-07-23 22:31:03 +0300

    feat: show major version on home

--- lib/features/home/presentation/home_wrapper_page.dart
+++ lib/features/home/presentation/home_wrapper_page.dart
@@ -1,16 +1,37 @@
+import 'package:datarunmobile/data/app_about_info.provider.dart';
 import 'package:datarunmobile/features/activity/presentation/activity_list_view.dart';
 import 'package:datarunmobile/features/home/presentation/drawer/app_drawer.dart';
 import 'package:datarunmobile/generated/l10n.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomeWrapperPage extends StatelessWidget {
+class HomeWrapperPage extends ConsumerWidget {
   const HomeWrapperPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final majorVersion = ref.watch(appAboutInfoProvider).when(
+          data: (appInfo) => appInfo.version.split('.').first,
+          error: (_, __) => null,
+          loading: () => null,
+        );
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(S.of(context).home),
+        title: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(S.of(context).home),
+            if (majorVersion != null) ...[
+              const SizedBox(width: 8),
+              Badge(
+                backgroundColor: Theme.of(context).colorScheme.tertiary,
+                textColor: Theme.of(context).colorScheme.onTertiary,
+                label: Text('v$majorVersion'),
+              ),
+            ],
+          ],
+        ),
       ),
       drawer: const AppDrawer(),
       body: SafeArea(child: const ActivityListView()),
